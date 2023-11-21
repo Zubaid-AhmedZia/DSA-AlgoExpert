fbfd24e1798c7a6bd2880708a7a0e674365d9517
Zubaid-AhmedZia <ziazubaidahmed@gmail.com>
2023-11-21 14:15:47 +0500

    better approach

--- twoNumberSum.js
+++ twoNumberSum.js
@@ -3,37 +3,53 @@
 //   up to the target sum, the function should return them in an array, in any
 //   order. If no two numbers sum up to the target sum, the function should return
 //   an empty array.
-// 
+//
 // Note that the target sum has to be obtained by summing two different integers
 //   in the array; you can't add a single integer to itself in order to obtain the
 //   target sum.
 // example:
 // input = [3, 5, -4, 8, 11, 1, -1, 6]
-// 
+//
 // output = [11,-1]
 
-// const array = [3, 5, -4, 8, 11, 1, 6, 4];
-// const targetSum = -1;
-var outputArr = [];
-function sum(a, b) {
-  return a + b;
-}
+const array = [3, 5, -4, 8, 11, 1, -1, 6];
+const targetSum = 10;
+// var outputArr = [];
+// function sum(a, b) {
+//   return a + b;
+// }
+// function twoNumberSum(array, targetSum) {
+//   for (let i = 0; i < array.length; i++) {
+//     let element = array[i];
+
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (sum(element, array[j]) === targetSum) {
+//         return [element, array[j]];
+
+//         outputArr.push(element, array[j]);
+//         break;
+//       }
+//       if (outputArr.length > 0) break;
+//     }
+//   }
+//   return outputArr;
+// }
+//GPT-4 answer
 function twoNumberSum(array, targetSum) {
-  for (let i = 0; i < array.length; i++) {
-    let element = array[i];
+  const nums = {}; // Using an object as a hash table
 
-    for (let j = i + 1; j < array.length; j++) {
-      if (sum(element, array[j]) === targetSum) {
-        return [element, array[j]];
-
-        outputArr.push(element, array[j]);
-        break;
-      }
-      if (outputArr.length > 0) break;
+  for (const num of array) {
+    const potentialMatch = targetSum - num;
+    if (potentialMatch in nums) {
+      return [potentialMatch, num]; // Found a pair
     }
+    nums[num] = true; // Store the number for future reference
+    console.log("nums ", nums);
   }
-  return outputArr;
+  
+
+  return []; // Return an empty array if no pair is found
 }
 
-// twoNumberSum(array, targetSum);
-exports.twoNumberSum = twoNumberSum;
+console.log(twoNumberSum(array, targetSum));
+// exports.twoNumberSum = twoNumberSum;
