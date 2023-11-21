314ed3f96c218f66b8d44761a6b86b0d080eaa39
Zubaid-AhmedZia <ziazubaidahmed@gmail.com>
2023-11-21 13:52:27 +0500

    qs added

--- twoNumberSum.js
+++ twoNumberSum.js
@@ -1,3 +1,17 @@
+// Write a function that takes in a non-empty array of distinct integers and an
+//   integer representing a target sum. If any two numbers in the input array sum
+//   up to the target sum, the function should return them in an array, in any
+//   order. If no two numbers sum up to the target sum, the function should return
+//   an empty array.
+// 
+// Note that the target sum has to be obtained by summing two different integers
+//   in the array; you can't add a single integer to itself in order to obtain the
+//   target sum.
+// example:
+// input = [3, 5, -4, 8, 11, 1, -1, 6]
+// 
+// output = [11,-1]
+
 // const array = [3, 5, -4, 8, 11, 1, 6, 4];
 // const targetSum = -1;
 var outputArr = [];
