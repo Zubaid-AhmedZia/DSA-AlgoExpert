fd423d7c9df97e4bdebcba1cda39505e9ca439d1
Zubaid-AhmedZia <ziazubaidahmed@gmail.com>
2023-11-21 19:31:04 +0500

    sortedSquare

--- sortedSquare.js
+++ sortedSquare.js
@@ -1,12 +1,10 @@
 let array = [1, 2, 3, 4, 5, 6];
 
-let sortedArray = []
+let sortedArray = [];
 
-
-
-function sortedSquare(array){
-array.map(el => sortedArray.push(el*el));
-return sortedArray;
+function sortedSquare(array) {
+  array.map((el) => sortedArray.push(el * el));
+  return sortedArray.sort((a, b) => a - b);
 }
 
 console.log(sortedSquare(array));
